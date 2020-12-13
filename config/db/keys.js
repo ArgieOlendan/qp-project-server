8909068927a9a91e6c7e24b3668985ef352235e0
argie <argieolndn@gmail.com>
2020-12-13 23:20:01 +0800

    add env keys

--- config/db/keys.js
+++ config/db/keys.js
@@ -1,5 +1,9 @@
 if (process.env.NODE_ENV === "production") {
   module.exports = require("./keys_prod");
+} else if (process.env.NODE_ENV === "review") {
+  module.exports = require("./keys_review");
+} else if (process.env.NODE_ENV === "staging") {
+  module.exports = require("./keys_staging.js");
 } else {
   module.exports = require("./keys_dev");
 }
